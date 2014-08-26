de1423f62090eee49b23f783318b18047a6509c1
Paul Bone <paul@bone.id.au>
2014-08-26 12:57:37 +1000

    Improve mfcgi_threadtest
    mfcgi_threadtest.m:
        Added error checking.
    
    Makefile:
        Build mfcgi_threadtest in a parallel grade.

--- Makefile
+++ Makefile
@@ -1,6 +1,10 @@
 
-MMC=mmc
+MMC=mmc --use-grade-subdirs
 MTAGS=mtags
+
+# Currently this is the most suitable parallel grade for this application as
+# IO and concurrency don't interact well on the low level C backend.
+PARALLEL_GRADE=hlc.gc.par
 
 .PHONY : all
 TARGETS = libmfcgi.so mfcgi_test mfcgi_threadtest
@@ -10,8 +14,10 @@ mfcgi_test : $(wildcard *.m)
 	$(MMC) --make mfcgi_test
 
 mfcgi_threadtest : $(wildcard *.m)
-	$(MMC) --make mfcgi_threadtest
+	$(MMC) --make --grade $(PARALLEL_GRADE) mfcgi_threadtest
 
+# This library is still built in the default grade.  TODO: build this
+# library in mulitple grades.
 libmfcgi.so : $(wildcard *.m)
 	$(MMC) --make libmfcgi
 
--- mfcgi_threadtest.m
+++ mfcgi_threadtest.m
@@ -20,18 +20,39 @@
 :- import_module mfcgi.multi.
 
 main(!IO) :-
-  fcgx_init(_, !IO),
-  fcgx_init_request(_, Request, 0, 0, !IO),
-  fcgx_accept_r(_, Request, !IO),
-  fcgx_write(header, Request, _, !IO),
-  fcgx_get_param_r("QUERY_STRING", Request, MaybeQuery, !IO),
-  (MaybeQuery = yes(Query) ->
-     fcgx_write(Query, Request, _, !IO)
-     ;
-     fcgx_write("No Query\n", Request, _, !IO)
-  ),
-  fcgx_finish_r(Request, !IO),
-  main(!IO).
+  fcgx_init(Result0, !IO),
+  (
+    Result0 = yes,
+    fcgx_init_request(MaybeRequest, 0, 0, !IO),
+    (
+      MaybeRequest = yes(Request),
+      fcgx_accept_r(Result1, Request, !IO),
+      (
+        Result1 = yes,
+        fcgx_write(header, Request, _, !IO),
+        fcgx_get_param_r("QUERY_STRING", Request, MaybeQuery, !IO),
+        (
+          MaybeQuery = yes(Query),
+          fcgx_write(Query, Request, _, !IO)
+        ;
+          MaybeQuery = no,
+          fcgx_write("No Query\n", Request, _, !IO)
+        ),
+        fcgx_finish_r(Request, !IO),
+        main(!IO)
+      ;
+        Result1 = no,
+        fcgx_finish_r(Request, !IO)
+        % accept failed.
+      )
+    ;
+      MaybeRequest = no
+      % init request failed.
+    )
+  ;
+    Result0 = no
+    % Init fcgx failed.
+  ).
 
 
 
