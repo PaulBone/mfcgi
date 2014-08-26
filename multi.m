1e01d467baf7d956f3227805aca86726929e0961
Paul Bone <paul@bone.id.au>
2014-08-26 12:05:03 +1000

    Create a "request" type for requests rather than using c_pointer.
    It is more typesafe to use a Mercury data type for each foreign type, it
    prevents any user from mixing up different c pointer.  I've also returned
    the request inside a maybe() when it is created by fcgx_init_request which
    is more idomatic and also helps with safety.
    
    This change has also allowed me to remove the type casts in the C code as
    that's handled automatically by Mercury's FFI.
    
    multi.m:
        As above.

--- multi.m
+++ multi.m
@@ -11,28 +11,31 @@
 
 :- import_module bool.
 :- import_module io.
+:- import_module maybe.
 
 %--------------------------------------------------------------------------%
 % Thread safe interface.
 %--------------------------------------------------------------------------%
 
+:- type request.
+
 % finish request accepted by previous call to fcgx_accept
 % also frees any storage allocated by previous call
-:- pred fcgx_finish_r(c_pointer::in, io::di, io::uo) is det.
+:- pred fcgx_finish_r(request::in, io::di, io::uo) is det.
 
 % initialize FCGX library. This is called by fcgx_accept
 % but should be caleld when using fcgx_accept_r
 :- pred fcgx_init(bool::out, io::di, io::uo) is det.
 
 % accept new request from HTTP server, 0 is success, -1 is error (thread-safe)
-:- pred fcgx_accept_r(bool::out, c_pointer::in, io::di, io::uo) is det.
+:- pred fcgx_accept_r(bool::out, request::in, io::di, io::uo) is det.
 
-% Initialize an FCGX_Request for use with FCGX_Accept_r(), 0 is success
-:- pred fcgx_init_request(bool::out, c_pointer::out, int::in, int::in,
+% Initialize an FCGX_Request for use with FCGX_Accept_r()
+:- pred fcgx_init_request(maybe(request)::out, int::in, int::in,
   io::di, io::uo) is det.
 
 % writing string to buffer in FCGX_Request struct 
-:- pred fcgx_write(string::in, c_pointer::in, bool::out, io::di, io::uo) is det.
+:- pred fcgx_write(string::in, request::in, bool::out, io::di, io::uo) is det.
 
 :- implementation.
 
@@ -45,10 +48,14 @@
 
 %--------------------------------------------------------------------------%
 
+:- pragma foreign_type("C", request, "FCGX_Request*").
+
+%--------------------------------------------------------------------------%
+
 :- pragma foreign_proc("C", fcgx_finish_r(ReqDataPtr::in, _IO0::di, _IO::uo),
   [thread_safe, promise_pure, will_not_call_mercury, tabled_for_io],
   "
-   FCGX_Finish_r((FCGX_Request *)ReqDataPtr);
+   FCGX_Finish_r(ReqDataPtr);
   ").
 
 :- pragma foreign_proc("C", fcgx_init(Success::out, _IO0::di, _IO::uo),
@@ -61,25 +68,43 @@
   _IO0::di, _IO::uo),
   [thread_safe, promise_pure, will_not_call_mercury, tabled_for_io],
   "
-   Success = FCGX_Accept_r((FCGX_Request *)ReqDataPtr) >= 0 ? MR_YES : MR_NO;
+   Success = FCGX_Accept_r(ReqDataPtr) >= 0 ? MR_YES : MR_NO;
   ").
 
+%--------------------------------------------------------------------------%
+
+fcgx_init_request(MaybeRequest, Sock, Flags, !IO) :-
+    do_init_request(Result, Request, Sock, Flags, !IO),
+    (
+        Result = yes,
+        MaybeRequest = yes(Request)
+    ;
+        Result = no,
+        MaybeRequest = no
+    ).
+
+:- pred do_init_request(bool::out, request::out, int::in, int::in,
+    io::di, io::uo) is det.
+
 :- pragma foreign_proc("C",
-    fcgx_init_request(Success::out, Request::out, Sock::in, Flags::in,
+    do_init_request(Success::out, Request::out, Sock::in, Flags::in,
         _IO0::di, _IO::uo),
     [promise_pure, will_not_call_mercury, tabled_for_io],
 "
     FCGX_Request *r = MR_NEW(FCGX_Request);
 
     Success = FCGX_InitRequest(r, Sock, Flags) == 0 ? MR_YES : MR_NO;
-    Request = (MR_Word)r;
+    Request = r;
 ").
+
+%--------------------------------------------------------------------------%
 
 :- pragma foreign_proc("C", fcgx_write(Str::in, Request::in, Success::out,
   _IO0::di, _IO::uo),
   [promise_pure, will_not_call_mercury, tabled_for_io],
   "
-   Success = FCGX_PutStr(Str, strlen(Str),
-       ((FCGX_Request *)Request)->out) >= 0 ? MR_YES : MR_NO;
+   Success = FCGX_PutStr(Str, strlen(Str), (Request)->out)
+                >= 0 ? MR_YES : MR_NO;
   ").
 
+%--------------------------------------------------------------------------%
